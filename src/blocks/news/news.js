eca4d3affa2c596e013138633460bf4793e82077
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2018-04-10 14:39:53 +0300

    headline and news section

--- src/blocks/news/news.js
+++ src/blocks/news/news.js
@@ -1,6 +1,8 @@
 $('.news__slider').slick({
 	slidesToScroll: 1,
 	slidesToShow: 1,
-	 centerMode: true,
-	variableWidth: true
+	centerMode: true,
+	variableWidth: true,
+	prevArrow: '<button type="button" class="news__slider-arrow news__slider-arrow--prev"><span>предыдущая</span></button>',
+	nextArrow: '<button type="button" class="news__slider-arrow news__slider-arrow--next"><span>следующая</span></button>'
 });
